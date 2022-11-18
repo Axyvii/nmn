a7be71c490476b07403d7de05d8185ca793760e0
Axyvii <axyvii1210@gmail.com>
2022-11-18 20:55:25 +0100

    index.js

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 // d.js classes
 const { Client, Events, GatewayIntentBits } = require('discord.js');
-const { DISCORD_TOKEN } = require('./process.env');
+const { token } = require('./config.json');
 
 // create a new instance
-const client = new Client({ intents: [GatewayIntentBits.Guilts] });
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 // once bot is ready
 client.once(Events.ClientReady, c => {
 	console.log('Bot is up! Logged in as ${c.user.tag}');
 });
 
-client.login(DISCORD_TOKEN);
+client.login(token);
